e8a49859d3382fbd968d7a6d111ca645ab7098f5
a1230421 <30762493+a1230421@users.noreply.github.com>
2022-09-30 23:43:05 +0800

    Delete js/static/js directory

--- js/static/js/tj.js
+++
@@ -1,2 +0,0 @@
-document.write('<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
-<script>LA.init({id: "Jgd8qNw6O8ryfF5N",ck: "Jgd8qNw6O8ryfF5N",autoTrack:true,hashMode:true})</script>')
